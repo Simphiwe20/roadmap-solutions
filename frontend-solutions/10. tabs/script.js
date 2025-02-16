1a35103103c4e982d91cdd4a60232d7cfc847ccb
Simphiwe Makhathini <gsimphiwe212@gmail.com>
2025-02-16 21:39:07 +0200

    simple tab done'

--- frontend-solutions/10. tabs/script.js
+++ frontend-solutions/10. tabs/script.js
@@ -1,29 +1,24 @@
 const tabs = document.querySelectorAll(".tab")
 const contents = document.querySelectorAll("#content")
-const previousIndex = undefined
+let previousIndex = 0
+let tabIndex = 0
 
 tabs.forEach((tab, indx) => {
     tab.addEventListener("click", (Ev) => {
         contents.forEach((content, _index) => {
-            console.log("HIDDEN: ", content.classList.value)
             let isToHide = content.classList.value.includes('display-block')
             let isToUnhide = content.classList.value.includes('display-none') && _index == indx
-            if (isToHide && !(previousIndex == _index && isToHide)) {
-                // console.log("contents: ", contents)
+            if (isToHide) {
+                if (previousIndex == indx && previousIndex == _index) return
                 content.classList.add("display-none")
                 content.classList.remove("display-block")
-                debugger
             } else if (isToUnhide) {
-                console.log("contents: ", contents)
+                previousIndex = _index
+                tabIndex = indx
                 content.classList.remove("display-none")
                 content.classList.add("display-block")
-                debugger
-
             }
-            console.log(`TAB INDEX: ${indx} and CONTENT index: ${_index}`)
-
         })
+
     })
 })
-
-console.log("TABS: ", tabs)
